7b8abf77ac43c23503a9755a2b5b26a4594c1852
Roy Ingar Hansen <roy@aentera.com>
2018-04-16 23:39:29 +0200

    Fixed publish script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,15 @@ cd ..
 dotnet restore
 dotnet publish -r linux-arm
 
-scp -r bin/Debug/netcoreapp2.0/linux-arm/publish/* pi@192.168.1.12:Apps/suvido
+Robocopy.exe bin/Debug/netcoreapp2.0/linux-arm/publish/ s:/brewtal -mir
+echo 'Going to kill app if running and then restart it. Type password and Ctrl+C when app is running.'
+ssh pi@192.168.1.12 'sudo kill $(ps -A | pgrep -i brewtal) | (cd /share/brewtal/; sudo ./brewtal) &'
+
+
+#scp -r bin/Debug/netcoreapp2.0/linux-arm/publish/* pi@192.168.1.12:Apps/suvido
+
+
+
 
 #rsync -avh -e ssh bin/Debug/netcoreapp2.0/linux-arm/publish/* pi@192.168.1.12:Apps/suvido
 
