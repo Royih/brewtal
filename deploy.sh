ab5cc7bc769bdee90b88e313e6fedf6efa48ab0d
Roy Ingar Hansen <rihansen@gmail.com>
2020-10-04 19:03:22 +0200

    Fixup

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-rm publish  -r -f
+rm -rf publish
 dotnet publish -r linux-arm -o publish
 
 #Robocopy.exe publish/ s:/brewtal -mir
 
-rsync -avzhe ssh --progress --delete ./publish/ pi@192.168.86.12:/home/pi/brewtal
+rsync -avzhe ssh --progress --delete --exclude appsettings.Production.json ./publish/ pi@192.168.86.12:/home/pi/brewtal
